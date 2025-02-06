5f68dece45852e93ce4b5cd2cc1b559d8a924e97
蕭昕瑜 <xiaoxinyu@xiaoxinyudeMacBook-Air.local>
2025-02-06 16:55:34 +0800

    route-baseon-auth

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import Router from './router/Router';
+import publicRoutes from './router/routers/publicRoutes';
+
+const App = () => {
+  const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
+
+  console.log(allRoutes);
+
+  return <Router allRoutes={allRoutes}></Router>;
+};
+
+export default App;
