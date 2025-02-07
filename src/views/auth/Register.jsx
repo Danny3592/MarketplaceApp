855054adba82245f775d44b604d8af18cb66999b
danny3592 <dannyxiao15@gmail.com>
2025-02-07 08:26:58 +0800

    tailwind-problem

--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,8 +1,17 @@
 
-
+import  './Register.css'
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="register">
+      <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
+        <div className="w-[350px text-[#ffffff] p-2]">
+          <div className="heading-container bg-[#6f68d1] p-4 rounded-md">
+            <h2 className="heading-3 text-xl mb-3 font-bold">Welcome to Ecommerce</h2>
+            <p className='text-sm'>Please Register yout account</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
